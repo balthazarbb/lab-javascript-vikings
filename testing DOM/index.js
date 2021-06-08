728e741d46c290c80c8bc898aa66df2b88e13d72
Bardia Biramzadeh <bardia-bb@gmx.de>
2021-06-08 20:09:43 +0200

    all itterations done

---
+++ testing DOM/index.js
@@ -0,0 +1,42 @@
+// 1: Get the node with the  main title
+
+// let header = document.querySelector ("title")
+
+// => <h1>Fruits and veggies</h1>
+// ----------------------------------------------
+
+// 2: Get the main title text
+
+// ... your code here
+
+// => Fruits and veggies
+// ----------------------------------------------
+
+// 3: Get all the nodes with the fruit items
+
+// ... your code here
+
+// => NodeList(4) [li.fruit-item, li.fruit-item, li.fruit-item, li.fruit-item]
+// ----------------------------------------------
+
+// 4: Get all the veggie items - the text
+// think about loops, innerText vs innerHTML
+
+// ... your code here
+
+// Broccoli Celery Potato Spinach
+// ----------------------------------------------
+
+// 5: Add a class 'best-fruit' to the 1st and the 3rd fruit
+// think about loops, ways to add classes to an element
+
+
+// => <li class="fruit-item best-fruit">Pear</li>
+// => <li class="fruit-item best-fruit">Plum</li>
+// ----------------------------------------------
+
+// 6: Remove class 'veggie-item' from 'spinach'
+
+// let myParagraphs = documment.getElementByClassName("veggie-item")
+
+// => ... <li class="veggie-love">Spinach</li
